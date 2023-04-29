c5fda6412a80341355dfe144c25f654a7837660e
Nursima Asilturk <nursima.asilturk99@gmail.com>
2023-04-30 00:53:56 +0300

    main-layout is created and routes of other pages are linked by url link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainDashboardComponent } from './Components/Layout/main-dashboard/main-dashboard.component';
+import { AboutComponent } from './Components/Pages/about/about.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
+import { ProjectComponent } from './Components/Pages/project/project.component';
+import { ContactComponent } from './Components/Pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainDashboardComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'projects',
+        component: ProjectComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainDashboardComponent } from './Components/Layout/main-dashboard/main-dashboard.component';
+import { NavbarComponent } from './Components/Layout/navbar/navbar.component';
+import { SidebarComponent } from './Components/Layout/sidebar/sidebar.component';
+import { AboutComponent } from './Components/Pages/about/about.component';
+import { ProjectComponent } from './Components/Pages/project/project.component';
+import { ContactComponent } from './Components/Pages/contact/contact.component';
+import { HomeComponent } from './Components/Pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainDashboardComponent, NavbarComponent, SidebarComponent, AboutComponent, ProjectComponent, ContactComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
