fbfa0a51bcd787e6932fe88b3c1471f8f6e7e373
Nursima Asilturk <nursima.asilturk99@gmail.com>
2023-05-24 17:20:24 +0300

    project detail page is done as version 1

--- src/app/Components/Pages/project/project.component.ts
+++ src/app/Components/Pages/project/project.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Project } from 'src/app/portfolio';
+import { ProjectService } from 'src/app/project.service';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
 })
 export class ProjectComponent implements OnInit {
-  slides: Project[] = [];
+  projects: Project[] = [];
   item: any;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private service: ProjectService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.projects = this.service.getAllProjects();
+    console.log(this.projects);
+  }
 
   redirectToProjectDetail(id: any) {
     this.router.navigate(['detail', id], { relativeTo: this.route });
